e648ac6fca00a43af489939071807ee20c207cb2
Joaquin Urbano Serra <kimbalear@gmail.com>
2024-10-15 16:13:08 +0100

    lang

---
+++ fosbrazil/lang/br/theme_fosbrazil.php
@@ -0,0 +1,51 @@
+<?php
+// Every file should have GPL and copyright in the header - we skip it in tutorials but you should not skip it for real.
+
+// This line protects the file from being accessed by a URL directly.
+defined('MOODLE_INTERNAL') || die();
+
+// A description shown in the admin theme selector.
+$string['choosereadme'] = 'El tema Circuitos é um tema filho do Boost. Ele adiciona a capacidade de fazer upload de fotos de fundo.';
+// The name of our plugin.
+$string['pluginname'] = 'Circuitos;
+// We need to include a lang string for each block region.
+$string['region-side-pre'] = 'Direita';
+// The name of the second tab in the theme settings.
+$string['advancedsettings'] = 'Configurações avançadas';
+// The brand colour setting.
+$string['brandcolor'] = 'Cor da marca';
+// The brand colour setting description.
+$string['brandcolor_desc'] = 'A cor de destaque.';
+// Name of the settings pages.
+$string['configtitle'] = 'Circuitos Configurações';
+// Name of the first settings tab.
+$string['generalsettings'] = 'Configurações gerais';
+// Preset files setting.
+$string['presetfiles'] = 'Arquivos de predefinição de tema adicionais';
+// Preset files help text.
+$string['presetfiles_desc'] = 'Os arquivos de predefinição podem ser usados para alterar drasticamente a aparência do tema. Consulte a <a href=https://docs.moodle.org/dev/Boost_Presets>documentação do Boost Presets</a> para obter informações sobre como criar e compartilhar seus próprios arquivos de predefinição, e veja o <a href=http://moodle.net/boost>repositório de predefinições</a> para predefinições que outras pessoas compartilharam.';
+// Preset setting.
+$string['preset'] = 'Predefinição de tema';
+// Preset help text.
+$string['preset_desc'] = 'Escolha uma predefinição para alterar amplamente a aparência do tema.';
+// Raw SCSS setting.
+$string['rawscss'] = 'Raw SCSS';
+// Raw SCSS setting help text.
+$string['rawscss_desc'] = 'Use este campo para fornecer código SCSS ou CSS que será injetado no final da folha de estilo.';
+// Raw initial SCSS setting.
+$string['rawscsspre'] = 'Raw Inicial SCSS';
+// Raw initial SCSS setting help text.
+$string['rawscsspre_desc'] = 'Neste campo, você pode fornecer código SCSS de inicialização, que será injetado antes de qualquer outra coisa. Na maioria das vezes, você usará esta configuração para definir variáveis.';
+
+// Footer
+
+$string['footer_menu1Item1'] = 'Política de Privacidade';
+$string['footer_menu1Item2'] = 'Sobre o Circuitos';
+$string['footer_menu1Item3'] = 'Contacto';
+
+
+$string['footer_orgData'] = 'POPULATION SERVICES INTERNATIONAL FOS FEMINISTA';
+$string['footer_contactNumberLb'] = 'Call:';
+
+$string['footer_links'] = '';
+$string['footer_CPRS'] = '';
