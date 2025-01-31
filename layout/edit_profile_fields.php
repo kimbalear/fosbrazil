fc8a0ca3decdf24a81ff6f5828371ce46df4adcc
Maybelline Portillo <portillom634@gmail.com>
2025-01-31 16:58:43 -0600

    update

--- layout/edit_profile_fields.php
+++ layout/edit_profile_fields.php
@@ -25,14 +25,49 @@
 defined('MOODLE_INTERNAL') || die();
 
 require_once($CFG->libdir . '/behat/lib.php');
+require_once($CFG->dirroot . '/course/lib.php');
 
 // Add block button in editing mode.
 $addblockbutton = $OUTPUT->addblockbutton();
 
-$extraclasses = [];
-$bodyattributes = $OUTPUT->body_attributes($extraclasses);
+//user_preference_allow_ajax_update('drawer-open-index', PARAM_BOOL);
+//user_preference_allow_ajax_update('drawer-open-block', PARAM_BOOL);
+
+$drawerOpenIndex = get_user_preferences('drawer-open-index', false);
+$drawerOpenBlock = get_user_preferences('drawer-open-block', false);
+
+set_user_preference('drawer-open-index', $drawerOpenIndex);
+set_user_preference('drawer-open-block', $drawerOpenBlock);
+
+if (isloggedin()) {
+    $courseindexopen = (get_user_preferences('drawer-open-index', true) == true);
+    $blockdraweropen = (get_user_preferences('drawer-open-block') == true);
+} else {
+    $courseindexopen = false;
+    $blockdraweropen = false;
+}
+
+if (defined('BEHAT_SITE_RUNNING')) {
+    $blockdraweropen = true;
+}
+
+$extraclasses = ['uses-drawers'];
+if ($courseindexopen) {
+    $extraclasses[] = 'drawer-open-index';
+}
+
 $blockshtml = $OUTPUT->blocks('side-pre');
 $hasblocks = (strpos($blockshtml, 'data-block=') !== false || !empty($addblockbutton));
+if (!$hasblocks) {
+    $blockdraweropen = false;
+}
+$courseindex = core_course_drawer();
+if (!$courseindex) {
+    $courseindexopen = false;
+}
+
+$bodyattributes = $OUTPUT->body_attributes($extraclasses);
+$forceblockdraweropen = $OUTPUT->firstview_fakeblocks();
 
 $secondarynavigation = false;
 $overflow = '';
@@ -49,7 +84,7 @@ if ($PAGE->has_secondary_navigation()) {
 $primary = new core\navigation\output\primary($PAGE);
 $renderer = $PAGE->get_renderer('core');
 $primarymenu = $primary->export_for_template($renderer);
-$buildregionmainsettings = !$PAGE->include_region_main_settings_in_header_actions()  && !$PAGE->has_secondary_navigation();
+$buildregionmainsettings = !$PAGE->include_region_main_settings_in_header_actions() && !$PAGE->has_secondary_navigation();
 // If the settings menu will be included in the header then don't add it here.
 $regionmainsettingsmenu = $buildregionmainsettings ? $OUTPUT->region_main_settings_menu() : false;
 
@@ -62,17 +97,25 @@ $templatecontext = [
     'sidepreblocks' => $blockshtml,
     'hasblocks' => $hasblocks,
     'bodyattributes' => $bodyattributes,
+    'courseindexopen' => $courseindexopen,
+    'blockdraweropen' => $blockdraweropen,
+    'courseindex' => $courseindex,
     'primarymoremenu' => $primarymenu['moremenu'],
     'secondarymoremenu' => $secondarynavigation ?: false,
     'mobileprimarynav' => $primarymenu['mobileprimarynav'],
     'usermenu' => $primarymenu['user'],
     'langmenu' => $primarymenu['lang'],
+    'forceblockdraweropen' => $forceblockdraweropen,
     'regionmainsettingsmenu' => $regionmainsettingsmenu,
     'hasregionmainsettingsmenu' => !empty($regionmainsettingsmenu),
-    'headercontent' => $headercontent,
     'overflow' => $overflow,
-    'addblockbutton' => $addblockbutton
+    'headercontent' => $headercontent,
+    'addblockbutton' => $addblockbutton,
+    'logofooter' => $OUTPUT->image_url('logo/logo_secundary_dark', 'theme_fosbrazil'),
+    'grap_dsgn_footer' => $OUTPUT->image_url('graphic_design/graf_1', 'theme_fosbrazil'),
+    'privacy_policy_url' => new moodle_url('/theme/fosbrazil/privacy_policy.php'),
+    'about_url' => new moodle_url('/theme/fosbrazil/about.php'),
+    'contact_url' => new moodle_url('/theme/fosbrazil/contact.php')
 ];
 
-echo $OUTPUT->main_content();
 echo $OUTPUT->render_from_template('theme_fosbrazil/edit_profile_fields', $templatecontext);
