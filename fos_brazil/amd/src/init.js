464cf737fe90879a1f1b9ad4b13472863807065f
Joaquin Urbano Serra <kimbalear@gmail.com>
2025-04-03 11:26:22 +0100

    max-height on the login page

---
+++ fos_brazil/amd/src/init.js
@@ -0,0 +1,10 @@
+export const init = () => {
+    document.getElementById("fitem_id_profile_field_occupation_other").style.display = "none";
+    document.getElementById("fitem_id_profile_field_gender_other").style.display = "none";
+    document.getElementById("fitem_id_profile_field_occupation_socialmedia1").style.display = "none";
+    document.getElementById("fitem_id_profile_field_occupation_socialmedia2").style.display = "none";
+    document.getElementById("fitem_id_profile_field_occupation_socialmedia3").style.display = "none";
+    document.getElementById("fitem_id_profile_field_occupation_socialmedia4").style.display = "none";
+    document.getElementById("fitem_id_city").style.display = "none";
+    document.getElementById("fitem_id_country").style.display = "none";
+};
---
+++ fos_brazil/version.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Plugin version and other meta-data are defined here.
+ *
+ * @package     theme_fos_space1
+ * @copyright   2024 digital Mood  <findme@digital-mood.eu>
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'theme_fosbrazil';
+$plugin->release = '1.1';
+$plugin->version = 2025020038;
+$plugin->requires = 2022112800;
+$plugin->maturity = MATURITY_RC;
+$plugin->dependencies = [
+    'theme_boost' => 2022112800,
+];
