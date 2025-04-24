601f20c6a4f9519c10859fc7995c125b693889aa
Maybelline Portillo <45375747+MaybellineP@users.noreply.github.com>
2025-04-23 20:20:05 -0600

    Update version.php

--- version.php
+++ version.php
@@ -11,7 +11,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'theme_fosbrazil';
 $plugin->release = '1.1';
-$plugin->version = 2025020083;
+$plugin->version = 2025020084;
 $plugin->requires = 2022112800;
 $plugin->maturity = MATURITY_RC;
 $plugin->dependencies = [
